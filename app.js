50d7f3999f17d59d963b19e020db33aea54e01a5
iiio2 <gomexxxx71@gmail.com>
2021-04-04 16:21:31 +0600

    finalized.

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-// // Funcion Composition
+// Funcion Composition
 
-// let input = '   JavaScript   ';
-// let output = '<div>' + input + '</div>';
+let input = '   JavaScript   ';
+let output = '<div>' + input + '</div>';
 
-// const trim = (str) => str.trim();
+const trim = (str) => str.trim();
 
-// const wrapDiv = (str) => '<div>' + str + '</div>';
+const wrapDiv = (str) => '<div>' + str + '</div>';
 
-// const lowerCase = (str) => str.toLowerCase();
+const lowerCase = (str) => str.toLowerCase();
 
-// const transform = _.pipe(trim, lowerCase, wrapDiv);
+const transform = _.pipe(trim, lowerCase, wrapDiv);
 
-// const result = transform(input);
-// console.log(result);
+const result = transform(input);
+console.log(result);
 
-// // Currying
-// // instead of taking arguments, taking different functions;
+// Currying
+// instead of taking arguments, taking different functions;
 
-// function sayHello(a) {
-//   return function (b) {
-//     return a + b;
-//   };
-// }
+function sayHello(a) {
+  return function (b) {
+    return a + b;
+  };
+}
 
-// const fn = sayHello(3)(7);
-// console.log(fn);
+const fn = sayHello(3)(7);
+console.log(fn);
 
-// // Updating Objects
+// Updating Objects
 
-// const person = {
-//   name: 'John',
-//   address: {
-//     country: 'USA',
-//     city: 'San Francisco',
-//   },
-// };
+const person = {
+  name: 'John',
+  address: {
+    country: 'USA',
+    city: 'San Francisco',
+  },
+};
 
-// const updatedObj = Object.assign({}, person, { name: 'Bob', age: 20 });
+const updatedObj = Object.assign({}, person, { name: 'Bob', age: 20 });
 
-// console.log(updatedObj);
-// // in spread operation , it is shallow copying. Use spread operation in nested objects too.
+console.log(updatedObj);
+// in spread operation , it is shallow copying. Use spread operation in nested objects too.
 
-// const updatedObj = {
-//   ...person,
-//   address: {
-//     ...person.address,
-//   },
-//   name: 'Bob',
-// };
+const updatedObj = {
+  ...person,
+  address: {
+    ...person.address,
+  },
+  name: 'Bob',
+};
 
-// console.log(person);
+console.log(person);
 
-// // Updating objects
+// Updating objects
 const numbers = [1, 2, 3];
 
 const res = numbers.map((n) => (n === 2 ? 50 : n));
