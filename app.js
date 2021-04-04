ae9ef77d5c0b1c560f3089184a204b740d30bb26
iiio2 <gomexxxx71@gmail.com>
2021-04-04 16:20:48 +0600

    added updating arrays.

--- app.js
+++ app.js
@@ -26,27 +26,33 @@
 // const fn = sayHello(3)(7);
 // console.log(fn);
 
-// Updating Objects
+// // Updating Objects
 
-const person = {
-  name: 'John',
-  address: {
-    country: 'USA',
-    city: 'San Francisco',
-  },
-};
+// const person = {
+//   name: 'John',
+//   address: {
+//     country: 'USA',
+//     city: 'San Francisco',
+//   },
+// };
 
 // const updatedObj = Object.assign({}, person, { name: 'Bob', age: 20 });
 
 // console.log(updatedObj);
-// in spread operation , it is shallow copying. Use spread operation in nested objects too.
+// // in spread operation , it is shallow copying. Use spread operation in nested objects too.
 
-const updatedObj = {
-  ...person,
-  address: {
-    ...person.address,
-  },
-  name: 'Bob',
-};
+// const updatedObj = {
+//   ...person,
+//   address: {
+//     ...person.address,
+//   },
+//   name: 'Bob',
+// };
 
-console.log(person);
+// console.log(person);
+
+// // Updating objects
+const numbers = [1, 2, 3];
+
+const res = numbers.map((n) => (n === 2 ? 50 : n));
+console.log(res);
