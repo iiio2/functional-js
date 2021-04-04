329ab9579f74f28e957875ff45c109ada048cd2c
iiio2 <gomexxxx71@gmail.com>
2021-04-04 13:52:05 +0600

    updating objects.

--- app.js
+++ app.js
@@ -14,14 +14,39 @@
 // const result = transform(input);
 // console.log(result);
 
-// Currying
-// instead of taking arguments, taking different functions;
+// // Currying
+// // instead of taking arguments, taking different functions;
 
-function sayHello(a) {
-  return function (b) {
-    return a + b;
-  };
-}
+// function sayHello(a) {
+//   return function (b) {
+//     return a + b;
+//   };
+// }
 
-const fn = sayHello(3)(7);
-console.log(fn);
+// const fn = sayHello(3)(7);
+// console.log(fn);
+
+// Updating Objects
+
+const person = {
+  name: 'John',
+  address: {
+    country: 'USA',
+    city: 'San Francisco',
+  },
+};
+
+// const updatedObj = Object.assign({}, person, { name: 'Bob', age: 20 });
+
+// console.log(updatedObj);
+// in spread operation , it is shallow copying. Use spread operation in nested objects too.
+
+const updatedObj = {
+  ...person,
+  address: {
+    ...person.address,
+  },
+  name: 'Bob',
+};
+
+console.log(person);
