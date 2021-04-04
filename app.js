0976f8493613f4b2921f1d18e69c4dd45de8b030
iiio2 <gomexxxx71@gmail.com>
2021-04-04 13:30:54 +0600

    currying added.

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-// Funcion Composition
+// // Funcion Composition
 
-let input = '   JavaScript   ';
-let output = '<div>' + input + '</div>';
+// let input = '   JavaScript   ';
+// let output = '<div>' + input + '</div>';
 
-const trim = (str) => str.trim();
+// const trim = (str) => str.trim();
 
-const wrapDiv = (str) => '<div>' + str + '</div>';
+// const wrapDiv = (str) => '<div>' + str + '</div>';
 
-const lowerCase = (str) => str.toLowerCase();
+// const lowerCase = (str) => str.toLowerCase();
 
-const transform = _.pipe(trim, lowerCase, wrapDiv);
+// const transform = _.pipe(trim, lowerCase, wrapDiv);
 
-const result = transform(input);
-console.log(result);
+// const result = transform(input);
+// console.log(result);
+
+// Currying
+// instead of taking arguments, taking different functions;
+
+function sayHello(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+const fn = sayHello(3)(7);
+console.log(fn);
